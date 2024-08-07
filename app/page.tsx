11ade2954c8f2ba2a30d31d298d312e63fbab672
Sergey Georgiev <sgrgv_art@mail.ru>
2024-08-07 18:48:03 +0300

    Компонент Home представляет собой основную страницу, которая включает в себя:
    Контейнер с заголовком "Все пиццы".
    Верхнюю панель с компонентом TopBar.
    Основной контейнер с фильтрами (с помощью Filters) и областью для отображения списка товаров.
    Закомментированный код для отображения групп продуктов, который может быть раскомментирован и использован в будущем.
    Этот компонент служит для отображения основной страницы с заголовком, фильтрами и списком товаров, организованным в гибком макете.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import { Button } from "@/components/ui/button";
+import { Title, Container, TopBar, Filters } from "@/components/shared";
 
 export default function Home() {
-  return <h1>Главная</h1>;
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+
+      <TopBar />
+      <Container className="mt-10 pb-14 ">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-col gap-16">
+            {/* <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+            <ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]} /> */}
+            Список товаров
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
