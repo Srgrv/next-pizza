857b355d28328afbf7d384565be55f8642162a07
Sergey Georgiev <sgrgv_art@mail.ru>
2024-08-20 21:48:30 +0300

    Создан компонент, который представляет собой группу продуктов с заголовком, сеткой продуктов и отслеживанием видимости для установки активной категории.

---
+++ components/shared/products-group-list.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React from "react";
+
+// useIntersection из react-use — хук для отслеживания видимости элемента на экране.
+import { useIntersection } from "react-use";
+
+// useCategoryStore — хук для работы с состоянием категории из вашего Zustand стора.
+import { useCategoryStore } from "@/store/category";
+
+// utils
+import { cn } from "@/lib/utils";
+
+// components
+import { Title } from "./title";
+import { ProductCard } from "./product-cart";
+
+interface IProps {
+  title: string;
+  items: any[];
+  className?: string;
+  listClassName?: string;
+  categoryId: number;
+}
+
+export const ProductsGroupList: React.FC<IProps> = ({
+  title,
+  items,
+  className,
+  listClassName,
+  categoryId,
+}) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  // intersectionRef — реф, который привязывается к элементу для отслеживания его видимости.
+  const intersectionRef = React.useRef(null);
+  // useIntersection — хук для отслеживания видимости элемента относительно окна просмотра (viewport). threshold: 1 означает, что элемент должен быть полностью видим (100%), чтобы событие сработало.
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  React.useEffect(() => {
+    console.log(intersection);
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting, title]);
+
+  return (
+    <div className={className} id={title} ref={intersectionRef}>
+      <Title text={title} size="lg" className="font-extrabold mb-5" />
+      <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
+        {items.map((product, i) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            imageUrl={product.imageUrl}
+            price={product.items[0].price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
