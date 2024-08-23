c450d2024becd3b6ebf07e22e209b688456e5d92
Sergey Georgiev <sgrgv_art@mail.ru>
2024-08-23 13:13:52 +0300

    Изменения: - В обновленной версии cats теперь содержит объекты с id и name, а не просто строки. - Состояние activeId теперь извлекается из Zustand хранилища с помощью хука useCategoryStore. - Добавлен атрибут href к <a> элементам.

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,38 +1,43 @@
+"use client";
+
 //используется для объединения и условного применения CSS-классов, похожим образом как clsx или classnames.
 import { cn } from "@/lib/utils";
 import React from "react";
+
+// store-hooks
+import { useCategoryStore } from "@/store/category";
 
 interface IProps {
   className?: string;
 }
 
 const cats = [
-  "Пиццы",
-  "Комбо",
-  "Закуски",
-  "Коктейли",
-  "Кофе",
-  "Напитки",
-  "Десерты",
+  { id: 1, name: "Пиццы" },
+  { id: 2, name: "Комбо" },
+  { id: 3, name: "Закуски" },
+  { id: 4, name: "Коктейли" },
+  { id: 5, name: "Кофе" },
+  { id: 6, name: "Напитки" },
+  { id: 7, name: "Десерты" },
 ];
 
-const activeIndex = 0;
-
 export const Categories: React.FC<IProps> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map((cat, index) => (
+      {cats.map(({ name, id }, index) => (
         <a
-          key={`${cat}_${index}`}
+          key={`${name}_${index}`}
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
-            activeIndex === index &&
+            categoryActiveId === id &&
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
+          href={`/#${name}`}
         >
-          <button>{cat}</button>
+          <button>{name}</button>
         </a>
       ))}
     </div>
