73a6b33e091e0584cd5fd637074d9d0ff6d407f7
Sergey Georgiev <sgrgv_art@mail.ru>
2024-08-28 16:18:33 +0300

    Добавлены ProductsCard и ProductsGroupList

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,3 +1,5 @@
+// это синтаксис, позволяющий экспортировать компоненты (или любые другие сущности) из указанных файлов. Это удобно для создания единой точки входа для всех компонентов, что упрощает их импорт в других частях приложения.
+
 export { Container } from "./container";
 export { Header } from "./header";
 export { Title } from "./title";
@@ -7,3 +9,5 @@ export { TopBar } from "./top-bar";
 export { Filters } from "./filters";
 export { RangeSlider } from "./range-slider";
 export { FilterCheckbox } from "./filter-checkbox";
+export { ProductCard } from "./product-cart";
+export { ProductsGroupList } from "./products-group-list";
