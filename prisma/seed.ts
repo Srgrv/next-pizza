d5f5a27b6e785ae45d1e35a46b998d757f835686
Sergey Georgiev <sgrgv_art@mail.ru>
2024-08-30 14:38:31 +0300

    Дополнения: 27. contants.ts. Создаем ингредиенты, продукты и экспортируем 28. seed.ts. Создаем функции, которые будут вызываться для заполнения ингредиентов 29. seed.ts. Создаем функции для заполнения пиццы 30. seed.ts. Создаем функцию, которая генерирует случайное десятичное число в указанном диапазоне min и max с одним десятичным знаком. 31. seed.ts. Создаем функцию, которая создает объект продукта с заданными характеристиками и случайно сгенерированной ценой. 32. seed.ts. Создаем функции, которые будут вызываться для заполнения ProductItem 33. seed.ts. Дополняем функции для удаления вновь созданной базы данных 34. package.json. Запускаем команду prisma:seed (очищение, а затем генерация базы данных) 35. seed.ts. Создаем функции для заполнения нескольких корзин 36. seed.ts. Создаем функции для заполнения корзины 37. seed.ts. Дополняем функции для удаления Cart, CartItem 38. package.json. Запускаем команду prisma:seed (очищение, а затем генерация базы данных)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,32 @@ import { hashSync } from "bcrypt";
 import { prisma } from "./prisma-client";
 
 // constants
-import { categories } from "./contants";
+import { _ingredients, categories, products } from "./contants";
+import { Prisma } from "@prisma/client";
+import { connect } from "http2";
+
+// 30. seed.ts. Создаем функцию, которая генерирует случайное десятичное число в указанном диапазоне min и max с одним десятичным знаком.
+const randomDecimalNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+// 31. seed.ts. Создаем функцию, которая создает объект продукта с заданными характеристиками и случайно сгенерированной ценой.
+const generateProductItem = ({
+  productId,
+  pizzaType,
+  size,
+}: {
+  productId: number;
+  pizzaType?: number;
+  size?: number;
+}) => {
+  return {
+    productId,
+    price: randomDecimalNumber(190, 600),
+    pizzaType,
+    size,
+  } as Prisma.ProductItemUncheckedCreateInput;
+};
 
 async function up() {
   await prisma.user.createMany({
@@ -27,10 +52,132 @@ async function up() {
   await prisma.category.createMany({
     data: categories,
   });
+
+  await prisma.ingredient.createMany({
+    data: _ingredients,
+  });
+
+  await prisma.product.createMany({
+    data: products,
+  });
+
+  // 29. seed.ts. Создаем функции для заполнения пиццы
+  const pizza1 = await prisma.product.create({
+    data: {
+      name: "Пепперони фреш",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: _ingredients.slice(0, 5),
+      },
+    },
+  });
+
+  const pizza2 = await prisma.product.create({
+    data: {
+      name: "Сырная",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: _ingredients.slice(5, 10),
+      },
+    },
+  });
+
+  const pizza3 = await prisma.product.create({
+    data: {
+      name: "Чоризо фреш",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: _ingredients.slice(10, 40),
+      },
+    },
+  });
+
+  // 32. seed.ts. Создаем функции, которые будут вызываться для заполнения ProductItem
+  await prisma.productItem.createMany({
+    data: [
+      // Пицца "Пепперони фреш"
+      generateProductItem({ productId: pizza1.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 40 }),
+
+      // Пицца "Сырная"
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 40 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 40 }),
+
+      // Пицца "Чоризо фреш"
+      generateProductItem({ productId: pizza3.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 40 }),
+
+      // Остальные продукты
+      generateProductItem({ productId: 1 }),
+      generateProductItem({ productId: 2 }),
+      generateProductItem({ productId: 3 }),
+      generateProductItem({ productId: 4 }),
+      generateProductItem({ productId: 5 }),
+      generateProductItem({ productId: 6 }),
+      generateProductItem({ productId: 7 }),
+      generateProductItem({ productId: 8 }),
+      generateProductItem({ productId: 9 }),
+      generateProductItem({ productId: 10 }),
+      generateProductItem({ productId: 11 }),
+      generateProductItem({ productId: 12 }),
+      generateProductItem({ productId: 13 }),
+      generateProductItem({ productId: 14 }),
+      generateProductItem({ productId: 15 }),
+      generateProductItem({ productId: 16 }),
+      generateProductItem({ productId: 17 }),
+    ],
+  });
+
+  // 35. seed.ts. Создаем функции для заполнения нескольких корзин
+  await prisma.cart.createMany({
+    data: [
+      {
+        userId: 1,
+        totalAmount: 0,
+        token: "123",
+      },
+      {
+        userId: 2,
+        totalAmount: 0,
+        token: "222222",
+      },
+    ],
+  });
+
+  //36. seed.ts. Создаем функции для заполнения корзины
+  await prisma.cartItem.create({
+    data: {
+      quantity: 2,
+      cartId: 1,
+      productItemId: 1,
+      ingredients: {
+        connect: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+      },
+    },
+  });
 }
 
+// 33. seed.ts. Дополняем функции для удаления вновь созданной базы данных
 async function down() {
   await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
 }
 
 async function main() {
